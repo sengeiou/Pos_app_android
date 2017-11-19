57ad3c0c40bc9a0e6e3020a26dcfb1b857eef35e
782818448@qq.com <782818448@qq.com>
2017-11-19 22:55:46 +0800

    1119 打包失败修改

--- easeui/build/generated/source/r/androidTest/debug/android/support/test/espresso/R.java
+++ easeui/build/generated/source/r/androidTest/debug/android/support/test/espresso/R.java
@@ -8,6 +8,6 @@ package android.support.test.espresso;
 
 public final class R {
     public static final class string {
-        public static final int app_name = 0x7f050160;
+        public static final int app_name = 0x7f05013e;
     }
 }
--- easeui/build/generated/source/r/androidTest/debug/android/support/test/rule/R.java
+++ easeui/build/generated/source/r/androidTest/debug/android/support/test/rule/R.java
@@ -8,6 +8,6 @@ package android.support.test.rule;
 
 public final class R {
     public static final class string {
-        public static final int app_name = 0x7f050160;
+        public static final int app_name = 0x7f05013e;
     }
 }
